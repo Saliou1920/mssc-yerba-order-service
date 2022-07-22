b27d47d633bf11b07fde45adfc3338606717d350
Saliou1920 <diopsaliou45@gmail.com>
2022-07-22 18:03:01 -0300

    :tada: first commit

---
+++ src/main/java/io/saliou/msscyerbaorderservice/MsscYerbaOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package io.saliou.msscyerbaorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscYerbaOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscYerbaOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/saliou/msscyerbaorderservice/MsscYerbaOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.saliou.msscyerbaorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscYerbaOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
